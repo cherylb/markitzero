b192d39e54bc4d1eaf6d5b87c840cb32fb9649ad
Cheryl Bowersox <boxsox_2@yahoo.com>
2014-07-09 23:16:23 -0700

    week 6 quiz correct file

--- week6Rquiz.R
+++ week6Rquiz.R
@@ -1,6 +1,20 @@
-# 
 
-require(xml)
-beer.url = "http://greatbrewers.com/top-50/beers"
-beer <- readHTMLTable(beer.url, header = T, which = 1)
+#Copy Pizza Data in R
+pizzapath <- "C:/Users/Cheryl/Downloads/pizza-store-data.csv"
+pizza <- read.csv(file = pizzapath, header = T, stringsAsFactors = F)
+
+#View head of data
+print("Preview of pizza store data")
+print(head(pizza))
+
+
+#description of data
+#date:  Character, date of the observation
+#store: Character, name of the pizza store
+#pizzas: integer, number of pizzas sold
+#customers:  integer, number of customers
+  
+# summary of data
+print("Summary of pizza store data")
+print(summary(pizza))
 
